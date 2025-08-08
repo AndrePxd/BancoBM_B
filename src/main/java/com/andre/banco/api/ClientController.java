f218662b76672b28f8b9bfd79544a1420b9aa21c
AndrePxd <andrepanique@gmail.com>
2025-08-07 23:38:58 -0400

    change profile function

--- src/main/java/com/andre/banco/api/ClientController.java
+++ src/main/java/com/andre/banco/api/ClientController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
-  @GetMapping("/health")
+  @GetMapping("/client")
   public String health() { return "OK"; }
 }
