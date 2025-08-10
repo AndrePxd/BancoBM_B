d06cd7980470cdf87f710b5ef49759ca2f2610f0
AndrePxd <andrepanique@gmail.com>
2025-08-10 18:59:39 -0400

    Test

--- src/test/java/com/andre/banco/banco_backend/BancoBackendApplicationTests.java
+++ src/test/java/com/andre/banco/banco_backend/BancoBackendApplicationTests.java
@@ -1,5 +1,9 @@
 package com.andre.banco.banco_backend;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +13,19 @@ class BancoBackendApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+  @Test
+  void sumaBasica_deberiaSer4() {
+    assertEquals(4, 2 + 2);
+  }
 
+  @Test
+  void cadenaEmpiezaConA() {
+    assertTrue("andres".startsWith("a"));
+  }
+
+  @Test
+  void listaNoVacia() {
+    var list = java.util.List.of(1,2,3);
+    assertFalse(list.isEmpty());
+  }
 }
